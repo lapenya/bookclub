3028dd789564bea2b100ab993f83983b13da77d2
Pablo Molina <pablo.molinacandel@gmail.com>
2012-06-17 19:47:17 +0200

    Setup defult host for mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,7 @@ Bookclub::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'http://cosicas.herokuapp.com/' }
+
 end
